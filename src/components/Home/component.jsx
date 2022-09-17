aa521fb1854fb8c9bc6342ba9579c032cceaea9d
loukrichiFouad97 <loukrichi21@gmail.com>
2022-09-17 23:59:30 +0100

    added responsiveness for home section

--- src/components/Home/component.jsx
+++ src/components/Home/component.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function HomeUI() {
   return (
     <section class="home" id="home">
-    <div class="home__text">
-      <h4>Hello</h4>
-      <h1>
-        I'M Fouad <br /> LOU<span>KRICHI</span>
-      </h1>
-      <h3>Front-End Developer</h3>
-      <a href="#" class="btn">
-        Hire Me
-      </a>
-    </div>
-  </section>
-  )
+      <div className="container">
+        <div class="home__text">
+          <h4>Hello</h4>
+          <h1>
+            I'M Fouad <br /> LOU<span>KRICHI</span>
+          </h1>
+          <h3>Front-End Developer</h3>
+          <a href="#" class="btn">
+            Hire Me
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default HomeUI
+export default HomeUI;
