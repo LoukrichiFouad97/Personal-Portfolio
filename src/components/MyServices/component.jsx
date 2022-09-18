8b9c0f7c2af3ebad633b1ef80ffc661c8cb97fa6
loukrichiFouad97 <loukrichi21@gmail.com>
2022-09-18 09:23:52 +0100

    added responsiveness for porfolio section.

--- src/components/MyServices/component.jsx
+++ src/components/MyServices/component.jsx
@@ -5,43 +5,42 @@ import ServiceTwo from "../../assets/serv2.png";
 import ServiceThree from "../../assets/serv3.png";
 
 function MyServicesUI() {
+  const services = [
+    {
+      title: "Mobile App Design",
+      image: ServiceOne,
+      body: " Lorem Ipsum Is Simply Dummy Text Of The Printing Andtypesetting Industry. Lorem Ipsum Has Been The Website Industry's Standard Dummy",
+    },
+    {
+      title: "Website Design",
+      image: ServiceTwo,
+      body: " Lorem Ipsum Is Simply Dummy Text Of The Printing Andtypesetting Industry. Lorem Ipsum Has Been The Website Industry's Standard Dummy",
+    },
+    {
+      title: "Video Editing",
+      image: ServiceThree,
+      body: " Lorem Ipsum Is Simply Dummy Text Of The Printing Andtypesetting Industry. Lorem Ipsum Has Been The Website Industry's Standard Dummy",
+    },
+  ];
+
+  const renderServices = services.map((service, index) => {
+    return (
+      <div className="box" key={index + 1}>
+        <img src={service.image} alt="" />
+        <h3>{service.title}</h3>
+        <p>{service.body}</p>
+      </div>
+    );
+  });
   return (
     <section class="services" id="services">
-      <div class="main-text">
-        <h2>My Services</h2>
-        <h4>Better Design, Better Expericences</h4>
-      </div>
-
-      <div class="services-content">
-        <div class="box">
-          <img src={ServiceOne} />
-          <h3>Mobile App Design</h3>
-          <p>
-            Lorem Ipsum Is Simply Dummy Text Of The Printing Andtypesetting
-            Industry. Lorem Ipsum Has Been The Website Industry's Standard Dummy
-            .
-          </p>
+      <div className="container">
+        <div class="main-text">
+          <h2>My Services</h2>
+          <h4>Better Design, Better Expericences</h4>
         </div>
 
-        <div class="box">
-          <img src={ServiceTwo} />
-          <h3>Website Design</h3>
-          <p>
-            Lorem Ipsum Is Simply Dummy Text Of The Printing Andtypesetting
-            Industry. Lorem Ipsum Has Been The Website Industry's Standard Dummy
-            .
-          </p>
-        </div>
-
-        <div class="box">
-          <img src={ServiceThree} />
-          <h3>Video Editing</h3>
-          <p>
-            Lorem Ipsum Is Simply Dummy Text Of The Printing Andtypesetting
-            Industry. Lorem Ipsum Has Been The Website Industry's Standard Dummy
-            .
-          </p>
-        </div>
+        <div class="services-content">{renderServices}</div>
       </div>
     </section>
   );
--- src/components/Portfolio/component.jsx
+++ src/components/Portfolio/component.jsx
@@ -8,91 +8,64 @@ import PortfolioImage5 from "../../assets/portfolio5.jpg";
 import PortfolioImage6 from "../../assets/portfolio6.jpg";
 
 function PortfolioUI() {
+  const projects = [
+    {
+      title: "Mobile app landing design & Services",
+      image: PortfolioImage1,
+      category: "Development",
+    },
+    {
+      title: "Mobile app landing design & Services",
+      image: PortfolioImage2,
+      category: "Development",
+    },
+    {
+      title: "Mobile app landing design & Services",
+      image: PortfolioImage3,
+      category: "Development",
+    },
+    {
+      title: "Mobile app landing design & Services",
+      image: PortfolioImage4,
+      category: "Development",
+    },
+    {
+      title: "Mobile app landing design & Services",
+      image: PortfolioImage5,
+      category: "Development",
+    },
+    {
+      title: "Mobile app landing design & Services",
+      image: PortfolioImage6,
+      category: "Development",
+    },
+  ];
+
+  const renderProjects = projects.map((project, index) => {
+    return (
+      <div className="row" key={index}>
+        <img src={project.image} />
+        <div className="main-row">
+          <div className="row-text">
+            <h6>{project.category}</h6>
+          </div>
+          <div className="row-icon">
+            <i className="bx bx-heart"></i>
+          </div>
+        </div>
+        <h3>{project.title}</h3>
+      </div>
+    );
+  });
+
   return (
     <section className="portfolio" id="portfolio">
-      <div className="main-text">
-        <h2>My Portfolio</h2>
-        <h4>Some Of My Distinguished Works</h4>
-      </div>
-
-      <div className="portfolio-content">
-        <div className="row">
-          <img src={PortfolioImage1} />
-          <div className="main-row">
-            <div className="row-text">
-              <h6>Development</h6>
-            </div>
-            <div className="row-icon">
-              <i className="bx bx-heart"></i>
-            </div>
-          </div>
-          <h3>Mobile app landing design & Services</h3>
+      <div className="container">
+        <div className="main-text">
+          <h2>My Portfolio</h2>
+          <h4>Some Of My Distinguished Works</h4>
         </div>
-
-        <div className="row">
-          <img src={PortfolioImage2} />
-          <div className="main-row">
-            <div className="row-text">
-              <h6>Development</h6>
-            </div>
-            <div className="row-icon">
-              <i className="bx bx-heart"></i>
-            </div>
-          </div>
-          <h3>Mobile app landing design & Services</h3>
-        </div>
-
-        <div className="row">
-          <img src={PortfolioImage3} />
-          <div className="main-row">
-            <div className="row-text">
-              <h6>Development</h6>
-            </div>
-            <div className="row-icon">
-              <i className="bx bx-heart"></i>
-            </div>
-          </div>
-          <h3>Mobile app landing design & Services</h3>
-        </div>
-
-        <div className="row">
-          <img src={PortfolioImage4} />
-          <div className="main-row">
-            <div className="row-text">
-              <h6>Development</h6>
-            </div>
-            <div className="row-icon">
-              <i className="bx bx-heart"></i>
-            </div>
-          </div>
-          <h3>Mobile app landing design & Services</h3>
-        </div>
-
-        <div className="row">
-          <img src={PortfolioImage5} />
-          <div className="main-row">
-            <div className="row-text">
-              <h6>Development</h6>
-            </div>
-            <div className="row-icon">
-              <i className="bx bx-heart"></i>
-            </div>
-          </div>
-          <h3>Mobile app landing design & Services</h3>
-        </div>
-
-        <div className="row">
-          <img src={PortfolioImage6} />
-          <div className="main-row">
-            <div className="row-text">
-              <h6>Development</h6>
-            </div>
-            <div className="row-icon">
-              <i className="bx bx-heart"></i>
-            </div>
-          </div>
-          <h3>Mobile app landing design & Services</h3>
-        </div>
+        <div className="portfolio__content">{renderProjects}</div>
       </div>
     </section>
   );
